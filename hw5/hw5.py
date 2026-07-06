b585f9be122149b145622f172f50f3e51a6b60d0
Masha <masha.ognyhchenko@icloud.com>
2026-07-06 09:34:47 +0200

    hw5

--- hw5/hw5.py
+++ hw5/hw5.py
@@ -1,3 +1,5 @@
+# Клас Character повинен приховувати hp (health points) від прямої зміни ззовні.
+#  Значення hp не повинно ставати від'ємним і не повинно перевищувати max_hp.
 class Character:
     def __init__(self, name: str, max_hp: int):
         self.name = name
@@ -8,15 +10,15 @@ class Character:
     def hp(self):
         return self.__hp
 
-    @hp.setter
-    def hp(self, value):
-        self.__hp = max(0, min(value, self.max_hp))
-
     def take_damage(self, amount: int) -> None:
-        self.hp -= amount
+        self.__hp -= amount
+        if self.__hp < 0:
+            self.__hp = 0
 
     def heal(self, amount: int) -> None:
-        self.hp += amount
+        self.__hp += amount
+        if self.__hp > self.max_hp:
+            self.__hp = self.max_hp
 
     def is_alive(self) -> bool:
-        return self.hp > 0
+        return self.__hp > 0
